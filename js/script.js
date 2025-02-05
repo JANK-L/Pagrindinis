63f676fd5dd0769f5cabbb92e1c38374eebff4ae
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-02-05 18:19:26 +0200

    diena18 baigta

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("vardas: Lukas ; amžius: 25 ; Pasirinktas programavimas, nes.\n");
+/*console.log("vardas: Lukas ; amžius: 25 ; Pasirinktas programavimas, nes.\n");
 
 console.log(
   "Žiema atėjo, sniegas krinta,\nŠaltas vėjas tyliai šnara.\nMedžiai stovi, šakos lūžta,\nBet širdyje – šiluma ir viltis.\n"
@@ -10,4 +10,15 @@ console.log("*\n" + "**\n" + "***\n");
 console.log(
   "pirma teksto eilute\n" + "kita tekso eilute\n" + "dar kita teksto eiltute"
 );
-alert("įvyko klaida");
+alert("įvyko klaida");*/
+document.getElementById("antraste").innerHTML = "pakeistas tekstas";
+
+console.log(
+  "+--------+--------+\n" +
+    "| Vardas | Amzius |\n" +
+    "+--------+--------+\n" +
+    "| Tomas  | 24     |\n" +
+    "| Jonas  | 26     |\n" +
+    "| Justė  | 25     |\n" +
+    "+--------+--------+\n"
+);
