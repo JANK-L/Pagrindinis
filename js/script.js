a5b49338ab2d8fcba11086dae15a50609337da27
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-02-05 19:07:57 +0200

    uzduotis 1/5

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// 1/5
+
+const vardas = "Vardenis";
+const pavarde = "Pavardenis";
+let amzius = 21;
+let ugis = 1.94;
+let svoris = 140;
+const aukstojiMokykla = "Žemasis Universitetas";
+const akademinesGrupesPavadinimas = "IE25/4";
+let kursas = 1;
+const studijuProgramosPavadinimas = "Industrijų ekonomika";
+let atsiskaitytuKredituSkaicius = 3;
+
+console.log(
+  `Vardas                : ${vardas}
+Pavardė               : ${pavarde}
+Amžius                : ${amzius} metai
+Ūgis                  : ${ugis} m
+Svoris                : ${svoris} Kg
+Aukštoji mokykla      : ${aukstojiMokykla}
+Grupė                 : ${akademinesGrupesPavadinimas}
+kursas                : ${kursas}
+Studijų grupė         : ${studijuProgramosPavadinimas}
+Atsiskaityti kreditai : ${atsiskaitytuKredituSkaicius}`
+);
