3102b00661fd9a7fd5c73705e07e7acc810f3b85
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-02-12 22:03:00 +0200

    pridetas pataisymas, kai skaicius nera ivestas

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function ar_lyginis(x) {
-  if (x % 2 == 0) {
-    newP.style.backgroundColor = "green";
-    newP.textContent = `Skaičius ${x} yra lyginis!`;
-  } else {
+  if (x % 2 != 0 || x === "") {
     newP.style.backgroundColor = "red";
     newP.textContent = `Skaičius ${x} yra nelyginis!`;
+  } else {
+    newP.style.backgroundColor = "green";
+    newP.textContent = `Skaičius ${x} yra lyginis!`;
   }
 }
 
 function atsakymas() {
   let skaicius = document.getElementById("skaicius").value;
-
+  console.log(skaicius);
   ar_lyginis(skaicius);
   newP.style.color = "white";
   document.body.appendChild(newP);
