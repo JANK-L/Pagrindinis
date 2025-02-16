9ea2a7cd40e531bf40b79b3da0314c481114368a
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-02-16 18:48:37 +0200

    padaryta uzrasu uzduotis

--- js/script.js
+++ js/script.js
@@ -1,21 +1,52 @@
-function ar_lyginis(x) {
-  if (x % 2 != 0 || x === "") {
-    newP.style.backgroundColor = "red";
-    newP.textContent = `Skaičius ${x} yra nelyginis!`;
-  } else {
-    newP.style.backgroundColor = "green";
-    newP.textContent = `Skaičius ${x} yra lyginis!`;
+document.querySelector("form").addEventListener("submit", naujasUzrasas);
+
+let uzrasuKiekis = 0;
+
+function naujasUzrasas(event) {
+  event.preventDefault();
+
+  let naujasTekstas = document.getElementById("newText").value.trim();
+
+  if (naujasTekstas == "") {
+    alert("Įrašykite tekstą.");
+    return;
+  }
+
+  sukurti(naujasTekstas, "div");
+}
+
+function sukurti(tekstas, element) {
+  let newDiv = document.createElement(element);
+
+  newDiv.textContent = tekstas;
+  newDiv.setAttribute("id", Date.now());
+
+  document.getElementById("uzrasai").appendChild(newDiv);
+
+  addButton(newDiv.id);
+
+  uzrasuKiekis++;
+
+  document.getElementById("tuscias").style.display = "none";
+}
+
+function addButton(id) {
+  let newButton = document.createElement("button");
+
+  newButton.textContent = "ištrinti";
+  newButton.setAttribute("onclick", "istrinti(event)");
+
+  document.getElementById(String(id)).appendChild(newButton);
+}
+
+function istrinti(event) {
+  let id = String(event.target.parentElement.id);
+  if (confirm("Ar tikrai norite ištrinti šį įrašą?")) {
+    document.getElementById(id).remove();
+    uzrasuKiekis--;
+  }
+
+  if (uzrasuKiekis == 0) {
+    document.getElementById("tuscias").style.display = "block";
   }
 }
-
-function atsakymas() {
-  let skaicius = document.getElementById("skaicius").value;
-  console.log(skaicius);
-  ar_lyginis(skaicius);
-  newP.style.color = "white";
-  document.body.appendChild(newP);
-}
-
-let newP = document.createElement("p");
-
-document.querySelector("button").addEventListener("click", atsakymas);
