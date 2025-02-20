eabbd2c37ade1347eeb93f54ab3ee8b73b3646f1
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-02-20 19:15:46 +0200

    papildoma uzduotis

--- js/script.js
+++ js/script.js
@@ -1,57 +1,14 @@
-let tekstoLaukas = document.getElementById("tekstas");
+let lenteles = document.querySelectorAll(".blokas .lentele");
 
-let isvalytiButton = document.querySelector("#išvalyti");
+lenteles.forEach((lentele) => {
+  lentele.addEventListener("click", () => pasirinktas(lentele));
+});
 
-let klaida = document.querySelector(".klaida");
+function pasirinktas(elementas) {
+  if (elementas.id === "") {
+    let aktivus = document.querySelector("#aktivus");
 
-let zodziaiDiv = document.querySelector(".zodziai");
-let zodziai = document.querySelector(".zodziai .kiekis");
-
-let simboliaiDiv = document.querySelector(".simboliai ");
-let simboliai = document.querySelector(".simboliai .kiekis");
-
-document.querySelector("#Analizuoti").addEventListener("click", analizuoti);
-isvalytiButton.addEventListener("click", išvalyti);
-tekstoLaukas.addEventListener("input", rodyti);
-
-function analizuoti() {
-  let newTekstas = tekstoLaukas.value.trim();
-
-  if (newTekstas === "") {
-    zodziaiDiv.classList.add("slepti");
-    simboliaiDiv.classList.add("slepti");
-    klaida.classList.remove("slepti");
-    isvalytiButton.classList.add("slepti");
-  } else {
-    zodziai.textContent = zodziu_kiekis(newTekstas);
-    zodziaiDiv.classList.remove("slepti");
-
-    simboliai.textContent = simboliu_kiekis(newTekstas);
-    simboliaiDiv.classList.remove("slepti");
-
-    klaida.classList.add("slepti");
-    isvalytiButton.classList.remove("slepti");
+    elementas.id = aktivus.id;
+    aktivus.id = "";
   }
-}
-function išvalyti() {
-  tekstoLaukas.value = "";
-  zodziaiDiv.classList.add("slepti");
-  simboliaiDiv.classList.add("slepti");
-  klaida.classList.add("slepti");
-  isvalytiButton.classList.add("slepti");
-}
-
-function zodziu_kiekis(tekstas) {
-  let kiekis = tekstas.split(" ");
-  return kiekis.length;
-}
-function simboliu_kiekis(tekstas) {
-  let kiekis = tekstas.split("");
-  return kiekis.length;
-}
-
-function rodyti() {
-  if (tekstoLaukas.value === "") {
-    isvalytiButton.classList.add("slepti");
-  } else isvalytiButton.classList.remove("slepti");
 }
