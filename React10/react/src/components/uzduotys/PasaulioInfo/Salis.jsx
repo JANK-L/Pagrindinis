767994828a4148c4c8a9e60891a3cde29c38ed55
jankevilu+git@gmail.com <jankevilu+git@gmail.com>
2025-04-24 21:18:58 +0300

    fix

--- React10/react/src/components/uzduotys/PasaulioInfo/Salis.jsx
+++ React10/react/src/components/uzduotys/PasaulioInfo/Salis.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Salis = ({ pavadinimas, zemynas }) => {
+  const arEuropa = (zemynas) => {
+    if (zemynas === "Europa") return "Jega";
+  };
+
   return (
     <div>
       <h3>{pavadinimas}</h3>
       <p>Priklauso zemynui {zemynas}</p>
+      {arEuropa(zemynas)}
     </div>
   );
 };
