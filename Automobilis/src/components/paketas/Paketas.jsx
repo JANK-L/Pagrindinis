564ec633cfd0deec62c00660b5a047d7d9de1412
JANK-L <jankevilu+git@gmail.com>
2025-05-19 18:25:00 +0300

    Empty

--- Automobilis/src/components/paketas/Paketas.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Paketas.css";
-const Paketas = () => {
-  return <div className="card">Paketas</div>;
-};
-
-export default Paketas;
--- Sandelys/src/components/Sandelys/Aukstas2.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Aukstas2 = () => {
-  return <div>A</div>;
-};
-
-export default Aukstas2;
